7468761ae6b5accb61c492389c9fe592165a20ff
Oliver Kill <oliver.kill@khk.ee>
2019-09-25 10:43:54 +0300

    Ülesanne 1 - võimalik lahendus

---
+++ 25.09.2019/ex_1.php
@@ -0,0 +1,7 @@
+<?php
+print "Eesnimi: Oliver<br>";
+print "Perenimi: Kill<br>";
+print 'Email: <a href="mailto:oliver.kill@khk.ee">oliver.kill@khk.ee</a><br>';
+print "Vanus: 19<br>";
+print "Pikkus(m): 1.83<br>";
+print "Kaal(kg): 56";
