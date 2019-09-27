f42a471ce476dd281effc8853c08d9e1bd4c324f
Oliver Kill <oliver.kill@khk.ee>
2019-09-27 09:52:55 +0300

    27.09.2019 Tunnitöö ja kokkuvõte

---
+++ 27.09.2019/kokkuvõte.md
@@ -0,0 +1,9 @@
+### 27.09.2019
+* php.net lehe pealt on võimalik leida kõik finktsioonid, mis directory-sse sisestatud
+* õppisime muutujate kasutamist
+* kasutasime ümardamise funktsiooni
+* kasutasime:
+```
+var_dump()
+```
+mis näitas vastava muutuja andmetüüpi
--- 27.09.2019/radius_do.php
+++ 27.09.2019/radius_do.php
@@ -1,8 +1,8 @@
 <?php
 $raadius = $_GET['raadius'];
 echo 'Sisestatud raadius on '.$raadius.'<br>';
-$pindalaS = $raadius * $raadius * 3.14;
-print $pindalaS."<br>";
-$ymbermootC = $raadius * 2 * 3.14;
-print $ymbermootC."<br>";
+$pindalaS = $raadius * $raadius * pi();
+print (round($pindalaS, 3))."<br>";
+$ymbermootC = $raadius * 2 * pi();
+print (round($ymbermootC, 3))."<br>";
 var_dump($ymbermootC);
