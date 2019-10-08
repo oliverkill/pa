75e80da65024234da8113fd354411a8e17675ef0
Oliver Kill <oliver.kill@khk.ee>
2019-10-08 09:13:45 +0300

    08.10.2019 If, else if, else

---
+++ 08.10.2019/pos_neg.php
@@ -0,0 +1,13 @@
+<?php
+$x = 0;
+var_dump($x>0);
+print '<br>';
+if ($x > 0) {
+  print 'Arv on positiivne';
+}
+else if ($x == 0) {
+    print 'Arv on 0';
+}
+else {
+    print 'Arv on negatiivne';
+}
