b6c5ef5944b29573e848d740d9f12b9c5d0b62c2
Oliver Kill <oliver.kill@khk.ee>
2019-10-17 09:56:24 +0300

    17.10.2019 asteriks column final 2.0

--- 17.10.2019/asteriks_table.php
+++ 17.10.2019/asteriks_table.php
@@ -5,7 +5,20 @@ for($rida=1; $rida<=5; $rida++){
         print '&nbsp;&nbsp;';
     }
     for ($col = 1; $col <=5;$col++) {
-        if ($rida >= $col) {
+        if ($rida >= $col) { //ainukene muutus
+            echo '&nbsp;*';
+        }
+    }
+    print '<br>';
+}
+print '<br>';
+for($rida=1; $rida<=5; $rida++){
+    print $rida.'&nbsp;';
+    if ($rida < 10) {
+        print '&nbsp;&nbsp;';
+    }
+    for ($col = 1; $col <=5;$col++) {
+        if ($rida <= $col) { //ainukene muutus
             echo '&nbsp;*';
         }
     }
