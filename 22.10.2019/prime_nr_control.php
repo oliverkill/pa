1d84292b8f592e4ed4094f3451015c7678aca7de
oliver.kill <killoliver08@gmail.com>
2019-10-22 10:52:05 +0300

    22.10.2019 prime_nr_control v2

--- 22.10.2019/prime_nr_control.php
+++ 22.10.2019/prime_nr_control.php
@@ -1,16 +1,29 @@
 <?php
 function isPrimeNumber ($number) {
     $result = '';
+    $bool = '';
     $divider = 2;
     while ($number % $divider != 0) {
         $divider++;
     }
     if ($number == $divider) {
         $result = $number.' on algarv<br>';
+        $bool = true;
+        return $bool;
     }
     else {
         $result = $number.' ei ole algarv<br>';
+        $bool = false;
+        return $bool;
     }
-    return $result;
+
 }
-print isPrimeNumber(rand(0,1000));
+function a ($val) {
+    if ($val) {
+        print 'Algarv';
+    }
+else {
+    print 'Ei ole algarv';
+}
+}
+a (isPrimeNumber(rand(0,1000)));
