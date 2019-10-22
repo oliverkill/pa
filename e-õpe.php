efa8e62a2f5a16e1fb3dc75c580531f3fe730f1c
oliver.kill <killoliver08@gmail.com>
2019-10-22 11:45:48 +0300

    21.10.2019 e-õpe

---
+++ e-õpe.php
@@ -0,0 +1,16 @@
+<?php
+function tervita1(){
+    return "Tere kylaline";
+}
+function tervita2(){
+    return "Tere kylaline";
+}
+
+echo tervita2();
+print '<br>';
+function tervita3($nimi){
+    return "Tere $nimi";
+}
+
+echo tervita3("Karin");
+?>
