94a9650d14d3c17ceadf829cea7ee1dab59c965e
oliver.kill <killoliver08@gmail.com>
2019-10-22 11:19:31 +0300

    22.10.2019 table v2

--- 22.10.2019/table.php
+++ 22.10.2019/table.php
@@ -1,11 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        table, th, td {
+            border: 1px #333 solid; border-collapse: collapse; text-align: center;
+        }
+        th, td {
+            width: 50px;
+            padding: 5px;
+        }
+    </style>
+    <title>Table</title>
+</head>
+<body>
+
 <?php
-//htv = header text value
-function tableHeader ($htv1, $htv2, $htv3, $htv4) {
-    print '<table>
-    <th>'.$htv1.'</th>
-    <th>'.$htv2.'</th>
-    <th>'.$htv3.'</th>
-    <th>'.$htv4.'</th>
-</table>';
+function tableHeader($val1, $val2, $val3, $val4) {
+    echo '<thead>';
+    echo '<tr>';
+    for($col = 1; $col < 5; $col++) {
+        echo '<th>' . ${val.$col} . '</th>';
+    }
+    echo '</tr>';
+    echo '</thead>';
 }
-tableHeader(a, b, c,d);
+function tableBody($rowNumber, $colNumber){
+    echo '<tbody>';
+    for($row = 1; $row <= $rowNumber; $row++){
+        echo '<tr>';
+        for($col = 1; $col <= $colNumber; $col++){
+            echo '<td>';
+            echo rand(10, 99);
+            echo '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</tbody>';
+}
+function table($rowNumber, $colNumber) {
+    echo '<table>';
+    //tableHeader('see', 'on', 'tabeli', 'päis');
+    tableBody($rowNumber, $colNumber);
+    echo '</table>';
+}
+table(5, 4);
