ee5b3b0e11c17f65f862b19203d1d7cde1861630
Oliver <killoliver08@gmail.com>
2019-10-24 17:20:33 +0300

    e-õpe 21.10 v2

--- 21.10.2019/e-õpe 21.10.php
+++ 21.10.2019/e-õpe 21.10.php
@@ -19,3 +19,31 @@ function tervita3($nimi)
 
 echo tervita3("Karin");
 
+echo min(11,21,32,43,54);
+echo max(11,21,32,43,54);
+
+$arvud = array(11,21,32,43,54);
+$arvud2 = array(60,72,42,34,45);
+
+echo max(max($arvud), max($arvud2));
+
+
+$arv = 3.456;
+
+echo ceil($arv);
+echo floor($arv);
+
+echo rand();
+echo mt_rand();
+echo rand(1,10);
+
+echo pow(5,2);
+echo pow(5,3);
+
+echo sqrt(9);
+
+echo pi();
+
+echo cos(0.8);
+
+echo deg2rad(30);
